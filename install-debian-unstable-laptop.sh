ea1acd4ac2bf7b6b19fc6042329ce2215fce3396
Nicklas Rudolfsson <66663564+nirucon@users.noreply.github.com>
2023-01-12 14:37:21 +0100

    Add files via upload

---
+++ install-debian-unstable-laptop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install git xserver-xorg xorg libx11-dev libxft-dev libxinerama-dev xorg-xinit xorg-xrandr xorg-xsetroot lxappearance arc-theme pcmanfm arandr nitrogen picom openssh cmus scrot gnupg pass mpv -yy
